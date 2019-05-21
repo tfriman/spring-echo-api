541ba273a26ec93f805ecb6e05ff065770d58c5d
Timo Friman <tfriman@redhat.com>
2019-05-21 09:50:53 +0300

    initial commit

---
+++ src/main/java/timosoft/echoapi/EchoapiApplication.java
@@ -0,0 +1,13 @@
+package timosoft.echoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoapiApplication.class, args);
+    }
+
+}
